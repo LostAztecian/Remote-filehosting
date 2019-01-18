1d1d2a2c1b6fd5add55590536720bbb33a2d2d26
Alexander <lostaztecian@mail.ru>
2019-01-18 20:00:31 +0300

    UI update

--- client/src/main/java/ru/stoliarenkoas/gb/filehosting/client/Controller.java
+++ client/src/main/java/ru/stoliarenkoas/gb/filehosting/client/Controller.java
@@ -1,4 +1,86 @@
 package ru.stoliarenkoas.gb.filehosting.client;
 
-public class Controller {
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    public void initialize(URL location, ResourceBundle resources) {
+        initializeWindowDragAndDropLabel();
+    }
+
+    @FXML
+    Label labelDragWindow;
+    @FXML
+    VBox mainVBox;
+
+    @FXML
+    private void login() {
+        System.out.println("login");
+    }
+
+    @FXML
+    private void auth(ActionEvent actionEvent) {
+        System.out.println("auth!");
+    }
+
+    public void btnShowModal(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Login.fxml"));
+            Parent root = loader.load();
+            LoginController lc = (LoginController) loader.getController();
+            lc.id = 100;
+            lc.backController = this;
+
+            stage.setTitle("JavaFX Autorization");
+            stage.setScene(new Scene(root, 400, 200));
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    double dragDeltaX, dragDeltaY;
+    public void initializeWindowDragAndDropLabel() {
+        Platform.runLater(() -> {
+            Stage stage = (Stage) mainVBox.getScene().getWindow();
+
+            labelDragWindow.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+                    // record a delta distance for the drag and drop operation.
+                    dragDeltaX = stage.getX() - mouseEvent.getScreenX();
+                    dragDeltaY = stage.getY() - mouseEvent.getScreenY();
+                }
+            });
+            labelDragWindow.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+                    stage.setX(mouseEvent.getScreenX() + dragDeltaX);
+                    stage.setY(mouseEvent.getScreenY() + dragDeltaY);
+                }
+            });
+        });
+    }
+
+    public void btnExit(ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }
--- client/src/main/java/ru/stoliarenkoas/gb/filehosting/client/Main.java
+++ client/src/main/java/ru/stoliarenkoas/gb/filehosting/client/Main.java
@@ -4,15 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../../../../../resources/main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+        primaryStage.setTitle("Filehosting Client");
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+
+        Scene scene = new Scene(root, 800, 600);
+        scene.setFill(Color.TRANSPARENT);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
